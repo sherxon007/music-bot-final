58bc178863b4d32488d69664a817a1e5269fd147
sherxon007 <91620888+sherxon007@users.noreply.github.com>
2025-12-23 16:08:13 +0500

    Add files via upload

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -7,12 +7,12 @@ from . import start, search, admin, ad_management, social, shazam
 def get_routers() -> list[Router]:
     """Get all handler routers."""
     return [
+        admin.router,
+        ad_management.router,
         start.router,
         social.router,
         shazam.router, # Handle media files
         search.router,
-        admin.router,
-        ad_management.router,
     ]
 
 
